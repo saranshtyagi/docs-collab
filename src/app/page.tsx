731b2af8c762db2658c3a93642e0673098b2eb13
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-12-05 00:06:55 +0530

    update Home component to replace button with link to document

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <p>Hello World</p>
-      <Button>Click Me</Button>
+      Click <Link href="/documents/123"><span className="text-blue-500 underline">here</span></Link> to go to the document id.
     </div>
   )
 }

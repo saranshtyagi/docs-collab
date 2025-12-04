a7ba46851a21170b2a30c01b3e07075e6f34b1bc
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-12-05 01:24:25 +0530

    update DocumentIdPage to render Editor component with styled container

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,3 +1,5 @@
+import { Editor } from "./editor";
+
 interface DocumentIdPageProps {
     params: Promise<{ documentId: string }>;
 };
@@ -6,7 +8,9 @@ const DocumentIdPage = async({params}: DocumentIdPageProps) => {
     const { documentId } = await params;
 
     return ( 
-        <div>Document ID: {documentId}</div>
+        <div className="min-h-screen bg-[#FAFBFD]">
+            <Editor />
+        </div>
     );
 }
  

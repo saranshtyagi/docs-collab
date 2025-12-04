f69d973489b6b145c87d2ec678f0dd393c00b520
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-12-05 00:08:12 +0530

    update Home component to add non-breaking spaces around the link text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      Click <Link href="/documents/123"><span className="text-blue-500 underline">here</span></Link> to go to the document id.
+      Click <Link href="/documents/123"><span className="text-blue-500 underline">&nbsp;here&nbsp;</span></Link> to go to the document id.
     </div>
   )
 }

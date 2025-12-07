8d52ef443cc80f976716982169ee03bb55e5562a
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-12-07 14:09:31 +0530

    feat: add LinkButton component for enhanced link insertion functionality

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -8,6 +8,7 @@ import {
   ChevronDownIcon,
   HighlighterIcon,
   ItalicIcon,
+  Link2Icon,
   ListTodoIcon,
   LucideIcon,
   MessageSquareIcon,
@@ -26,6 +27,9 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const FontFamilyButton = () => {
   const { editor } = useEditorStore();
@@ -185,6 +189,44 @@ const HighlightColorButton = () => {
   )
 }
 
+const LinkButton = () => {
+  const { editor } = useEditorStore();
+  const [value, setValue] = useState("");
+
+  const onChange = (href: string) => {
+    editor?.chain().focus().extendMarkRange("link").setLink({ href }).run();
+    setValue("");
+  }
+
+  return (
+    <DropdownMenu onOpenChange={(open) => {
+      if(open) {
+        setValue(editor?.getAttributes("link").href || "");
+      }
+    }}>  
+      <DropdownMenuTrigger asChild>
+        <button
+          className="h-7 w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+        >
+          <Link2Icon className="size-4" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-2.5 flex items-center gap-x-2">
+        <Input
+          placeholder="https://example.com"
+          value={value}
+          onChange={(e) => setValue(e.target.value)} 
+        />
+        <Button 
+          onClick={() => onChange(value)}
+        >
+          Apply
+        </Button>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
 interface ToolbarButtonProps {
   onClick?: () => void;
   isActive?: boolean;
@@ -305,7 +347,7 @@ export const Toolbar = () => {
       <TextColorButton />
       <HighlightColorButton />
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
-      {/* TODO: Link  */}
+      <LinkButton />
       {/* TODO: Image  */}
       {/* TODO: Align  */}
       {/* TODO: Line Height  */}

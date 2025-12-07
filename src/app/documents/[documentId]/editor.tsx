459ade134685d68aedf9db8281ff9d9226d990dd
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-12-07 14:09:24 +0530

    feat: add Link extension to Tiptap for enhanced text linking capabilities

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -15,6 +15,7 @@ import FontFamily from '@tiptap/extension-font-family'
 import TextStyle from '@tiptap/extension-text-style';
 import { Color } from '@tiptap/extension-color';
 import Highlight from '@tiptap/extension-highlight';
+import Link from '@tiptap/extension-link';
 
 import { useEditorStore } from '@/store/use-editor-store';
 
@@ -54,6 +55,11 @@ export const Editor = () => {
         },
         extensions: [
             StarterKit,
+            Link.configure({
+              openOnClick: false,
+              autolink: true,
+              defaultProtocol: 'https',
+            }),
             Color, 
             Highlight.configure({
               multicolor: true,

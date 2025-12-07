6d2fd7f5ac38f29a16fa64a346e7d4f129588245
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-12-07 14:41:52 +0530

    feat: add AlignButton component for text alignment options in the toolbar

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -4,6 +4,10 @@ import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
 import {
+  AlignCenterIcon,
+  AlignJustifyIcon,
+  AlignLeftIcon,
+  AlignRightIcon,
   BoldIcon,
   ChevronDownIcon,
   HighlighterIcon,
@@ -316,6 +320,60 @@ const ImageButton = () => {
   )
 }
 
+const AlignButton = () => {
+  const { editor } = useEditorStore();
+
+  const alignments = [
+    {
+      label: "Align Left", 
+      value: "left", 
+      icon: AlignLeftIcon, 
+    }, 
+    {
+      label: "Align Center",
+      value: "center",
+      icon: AlignCenterIcon
+    }, 
+    {
+      label: "Align Right",
+      value: "right",
+      icon: AlignRightIcon
+    }, 
+    {
+      label: "Justify", 
+      value: "justify",
+      icon: AlignJustifyIcon
+    }
+  ]
+
+  return (
+    <DropdownMenu>  
+      <DropdownMenuTrigger asChild>
+        <button
+          className="h-7 w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+        >
+          <AlignLeftIcon className="size-4" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        {alignments.map(({ label, value, icon: Icon}) => (
+          <button
+            key={value}
+            onClick={() => editor?.chain().focus().setTextAlign(value).run()}
+            className={cn(
+              "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+              editor?.isActive({ textAlign: value }) && "bg-neutral-200/80"
+            )}
+          >
+            <Icon className="size-4" />
+            <span className="text-sm">{label}</span>
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
 interface ToolbarButtonProps {
   onClick?: () => void;
   isActive?: boolean;
@@ -438,7 +496,7 @@ export const Toolbar = () => {
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       <LinkButton />
       <ImageButton />
-      {/* TODO: Align  */}
+      <AlignButton />
       {/* TODO: Line Height  */}
       {/* TODO: List */}
       {sections[2].map((item) => (
